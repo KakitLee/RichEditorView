4b5979fdbcdc11ac2506cefa2b76556b32e21055
Duy Vu <hoangduy308@gmail.com>
2017-12-03 10:47:37 +1100

    implement get selection range

--- RichEditorView/Assets/editor/rich_editor.js
+++ RichEditorView/Assets/editor/rich_editor.js
@@ -480,3 +480,27 @@ RE.getRelativeCaretYPosition = function() {
     
     return y;
 };
+
+RE.getSelectionCharOffsetsWithin = function() {
+    var start = 0, end = 0;
+    var sel, range, priorRange;
+    var element = document.getElementById("main");
+    if (typeof window.getSelection != "undefined") {
+        range = window.getSelection().getRangeAt(0);
+        priorRange = range.cloneRange();
+        priorRange.selectNodeContents(element);
+        priorRange.setEnd(range.startContainer, range.startOffset);
+        start = priorRange.toString().length;
+        end = start + range.toString().length;
+    } else if (typeof document.selection != "undefined" &&
+               (sel = document.selection).type != "Control") {
+        range = sel.createRange();
+        priorRange = document.body.createTextRange();
+        priorRange.moveToElementText(element);
+        priorRange.setEndPoint("EndToStart", range);
+        start = priorRange.text.length;
+        end = start + range.text.length;
+    }
+    var string = start + ":" + end;
+    return string;
+};
